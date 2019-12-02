513cd9de42e89f367ebef70d09401e2b46196b5e
Mike Thurgood <mikecthurgood@gmail.com>
2019-12-02 13:58:53 +0000

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-Country.create(name: "Laos",
-    description: "Modern-day Laos has its roots in the ancient Lao kingdom of Lan Xang, established in the 14th century under King FA NGUM. For 300 years Lan Xang had influence reaching into present-day Cambodia and Thailand, as well as over all of what is now Laos. After centuries of gradual decline, Laos came under the domination of Siam (Thailand) from the late 18th century until the late 19th century, when it became part of French Indochina. The Franco-Siamese Treaty of 1907 defined the current Lao border with Thailand. In 1975, the communist Pathet Lao took control of the government, ending a six-century-old monarchy and instituting a strict socialist regime closely aligned to Vietnam. A gradual, limited return to private enterprise and the liberalization of foreign investment laws began in 1988. Laos became a member of ASEAN in 1997 and the WTO in 2013.",
-    continent: "Southeast Asia",
-    population: 7234171,
-    climate: "tropical monsoon; rainy season (May to November); dry season (December to April)",
-    terrain: "mostly rugged mountains; some plains and plateaus")
+laos = Country.find_by(name: 'Laos')
+laos.code = 'la'
+laos.save
