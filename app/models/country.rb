8a245a333428064f223b8cd67431955e865916e3
Mike Thurgood <mikecthurgood@gmail.com>
2019-12-04 10:55:35 +0000

    nico's changes

--- app/models/country.rb
+++ app/models/country.rb
@@ -5,24 +5,25 @@ class Country < ApplicationRecord
     has_many :users, through: :user_countries
 
     def self.call_skyscanner(departureLocation, destination, departureDate, returnDate)
-        response = Unirest.post "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
-  headers:{
-    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    "x-rapidapi-key": "571c034a1amshba07a35df5f1f2bp163af5jsn9566c59b5b86",
-    "Content-Type" => "application/x-www-form-urlencoded"
-  },
-  parameters:{
-    "inboundDate" => returnDate,
-    "cabinClass" => "economy",
-    "children" => 0,
-    "infants" => 0,
-    "country" => "UK",
-    "currency" => "GBP",
-    "locale" => "en-US",
-    "originPlace" => departureLocation,
-    "destinationPlace" => destination,
-    "outboundDate" => departureDate,
-    "adults" => 1
-  }
+        response = Unirest.post("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0/",
+        headers = {
+            "X-RapidAPI-Host" => "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "X-RapidAPI-Key" => "571c034a1amshba07a35df5f1f2bp163af5jsn9566c59b5b86",
+            "Content-Type" => "application/x-www-form-urlencoded"
+        },
+        parameters = {
+            "inboundDate" => returnDate,
+            "cabinClass" => "economy",
+            "children" => 0,
+            "infants" => 0,
+            "country" => "UK",
+            "currency" => "GBP",
+            "locale" => "en-GB",
+            "originPlace" => departureLocation,
+            "destinationPlace" => destination,
+            "outboundDate" => departureDate,
+            "adults" => 1
+        })
+        # byebug
     end
 end
