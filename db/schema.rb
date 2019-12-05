cfce26ec9941b0a08e93fc029e46d76996b05f14
Mike Thurgood <mikecthurgood@gmail.com>
2019-12-05 11:00:51 +0000

    db updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_112112) do
+ActiveRecord::Schema.define(version: 2019_12_04_143534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_112112) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "code"
+    t.string "image_url"
   end
 
   create_table "user_badges", force: :cascade do |t|
